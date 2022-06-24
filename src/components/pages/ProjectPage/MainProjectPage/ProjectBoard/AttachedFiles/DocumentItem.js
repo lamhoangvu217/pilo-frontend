59b067ca8209eb5081a46f838ccbfa470f947427
lamhoangvu217 <lamhoangvu217@gmail.com>
2022-06-24 09:24:51 +0700

    final

--- src/components/pages/ProjectPage/MainProjectPage/ProjectBoard/AttachedFiles/DocumentItem.js
+++ src/components/pages/ProjectPage/MainProjectPage/ProjectBoard/AttachedFiles/DocumentItem.js
@@ -29,6 +29,28 @@ function DocumentItem({ file }) {
         </a>
       </>
     );
+  } else if (file.fileType === "application/vnd.openxmlformats-officedocument.wordprocessingml.document" || file.fileType === "application/msword") {
+    displayFile = (
+      <>
+        <a href={`${imageHost}/${file.filePath}`} target="_blank" className="flex flex-col items-center" download rel="noreferrer">
+          <img src="/images/word.png" className="w-10 h-10" alt="" />
+          <span className="text-xs text-gray-400 font-normal">
+            {file.fileName}
+          </span>
+        </a>
+      </>
+    );
+  } else if (file.fileType === "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet" || file.fileType === "application/vnd.ms-excel") {
+    displayFile = (
+      <>
+        <a href={`${imageHost}/${file.filePath}`} target="_blank" className="flex flex-col items-center" download rel="noreferrer">
+          <img src="/images/excelicon.png" className="w-10 h-10" alt="" />
+          <span className="text-xs text-gray-400 font-normal">
+            {file.fileName}
+          </span>
+        </a>
+      </>
+    );
   }
   return (
     <div className="flex flex-col mr-2 cursor-pointer mt-2 border-[1px] border-gray-300 p-6 rounded-lg justify-center items-center">
